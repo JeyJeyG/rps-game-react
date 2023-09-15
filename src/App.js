5edbf4a21affd325bece6c8694d46326b975b353
JeyJeyG <jeyjeygraham@gmail.com>
2023-09-15 10:32:58 +0000

    implementation of game logic

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import ChoiceButtons from "./Components/ChoiceButtons";
 import ResultMessage from "./Components/ResultMessage";
 
-const choices = [rock, paper, scissors];
+const choices = ['rock', 'paper', 'scissors'];
 
 function App(){
   const [playerChoice, setPlayerChoice] = useState('');
   const [computerChoice, setComputerChoice] = useState('');
   const [result, setResult] = useState('');
 
-  //......Rest of Code
+  const handlePlayerChoice = (choice) => {
+    // Implement the logic for handling the player's choice here
+  };
+
+  const handleRestart = () => {
+    // Implement the logic for restarting the game here
+  };
+
+  // ... Rest of your code
+
 
 return(
   <div>
